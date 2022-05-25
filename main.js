f4a3f5bfff6e306d7669f7c841b8453c9d0b274c
Waleed Hewaidy <waleedhewaidydev@gmail.com>
2022-05-25 14:54:49 +0300

    Finishing Navbar and Side Menu Functionality

---
+++ main.js
@@ -0,0 +1,12 @@
+// Implementing Sideling (close&open)
+
+const showMenuBtn = document.getElementById('hamburger')
+const closeMenuBtn = document.getElementById('closeMenuBtn')
+const sideMenu = document.querySelector('.side-links-cont')
+
+showMenuBtn.addEventListener('click',()=>{
+sideMenu.classList.add('showSideLinks')
+})
+closeMenuBtn.addEventListener('click',()=>{
+sideMenu.classList.remove('showSideLinks')
+})
