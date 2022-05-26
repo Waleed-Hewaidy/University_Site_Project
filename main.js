6cfd8b67abc7039d33efffb53e855d532ae68ba8
Waleed Hewaidy <waleedhewaidydev@gmail.com>
2022-05-26 19:03:50 +0300

    Closing Side Menu When Clicking Elsewhere

--- main.js
+++ main.js
@@ -3,11 +3,25 @@
 const showMenuBtn = document.getElementById('hamburger')
 const closeMenuBtn = document.getElementById('closeMenuBtn')
 const sideMenu = document.querySelector('.side-links-cont')
+const navContainer = document.querySelector('.nav-container')
+const heroContainer = document.querySelector('.heroContainer')
+
+
 
 showMenuBtn.addEventListener('click',()=>{
 sideMenu.classList.add('showSideLinks')
 })
+
 closeMenuBtn.addEventListener('click',()=>{
 sideMenu.classList.remove('showSideLinks')
 })
- 
+
+navContainer.addEventListener('click', (e)=>{
+    if(e.target !== showMenuBtn.firstElementChild){
+        sideMenu.classList.remove('showSideLinks')
+    }
+})
+
+heroContainer.addEventListener('click', ()=>{
+    sideMenu.classList.remove('showSideLinks')
+})
