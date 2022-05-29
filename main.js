2d0ca39176531d22a10a1382be26085b3f82a774
Waleed Hewaidy <waleedhewaidydev@gmail.com>
2022-05-29 12:07:30 +0300

    Added Contact Us Box next to Reviews Box

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ const reviews = [
       img:
         "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
       text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar .",
     },
     {
       id: 2,
